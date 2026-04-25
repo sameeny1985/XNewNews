504b246d073a39d65dccc0db095158e1258524ce
sameeny1985 <ameeny1said@gmail.com>
2026-04-25 05:15:15 +0200

    Update MY_SITE_URL to new domain

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ translator = Translator()
 
 TOKEN = os.environ.get("TOKEN")
 CHAT_ID = "@XNewNewsMavara"
-MY_SITE_URL = "https://fxanlytix-news-gga6.onrender.com"
+MY_SITE_URL = "https://xnewnews.onrender.com"
 DB_PATH = "news.db"
 
 # لیست منابع اصلاح شده برای پایداری
