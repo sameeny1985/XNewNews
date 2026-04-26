a4830ba35b4235a750292bcdf44a5c2d988df981
sameeny1985 <ameeny1said@gmail.com>
2026-04-26 08:12:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -260,4 +260,4 @@ def news_detail(news_id):
     abort(404)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
